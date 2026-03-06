9457b03a3248f77c08efe4f4b09fd7dbdcd3891e
alejandroudot <alexxudot@gmail.com>
2026-03-06 17:51:01 -0300

    feat:

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,29 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import viteCompression from 'vite-plugin-compression';
 
 // https://astro.build/config
 export default defineConfig({
+  devToolbar: {
+    enabled: false,
+  },
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [
+      tailwindcss(),
+      viteCompression({
+        algorithm: 'brotliCompress',
+        ext: '.br',
+        threshold: 10240,
+        deleteOriginFile: false,
+      }),
+      viteCompression({
+        algorithm: 'gzip',
+        ext: '.gz',
+        threshold: 10240,
+        deleteOriginFile: false,
+      }),
+    ],
     server: {
       watch: {
         usePolling: true,
